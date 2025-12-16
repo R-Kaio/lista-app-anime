3de160d8c59d0f5e6dcc82269aa6618ef276c962
R-Kaio <riancaxias@aluno.uespi.br>
2025-12-15 22:26:29 -0300

    criação de estrutura mvvm - models

---
+++ lib/models/anime_model.dart
@@ -0,0 +1,23 @@
+class Anime {
+  final int id;
+  final String title;
+  final String imageUrl;
+  final double? score;
+
+  Anime({
+    required this.id,
+    required this.title,
+    required this.imageUrl,
+    this.score,
+  });
+
+  // Factory: Cria uma instância de Anime a partir de um mapa JSON
+  factory Anime.fromJson(Map<String, dynamic> json) {
+    return Anime(
+      id: json['mal_id'],
+      title: json['title'],
+      imageUrl: json['images']['jpg']['image_url'],
+      score: json['score'] != null ? (json['score'] as num).toDouble() : 0.0,
+    );
+  }
+}
