4d38eca0ed01f6d5f92a2df4c46e4c24fce49690
R-Kaio <riancaxias@aluno.uespi.br>
2025-12-16 20:06:29 -0300

    adiciona pull-to-refresh e melhoria no estado de erro

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -36,47 +36,85 @@ class _HomeScreenState extends State<HomeScreen> {
           // Estado erro
           if (viewModel.error.isNotEmpty) {
             return Center(
-              child: Text(
-                viewModel.error,
-                style: const TextStyle(color: Colors.red, fontSize: 16),
-                textAlign: TextAlign.center,
+              child: Padding(
+                padding: const EdgeInsetsGeometry.all(16),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(
+                      Icons.error_outline,
+                      size: 60,
+                      color: Colors.red,
+                    ),
+                    const SizedBox(height: 16),
+                    const Text(
+                      'Ops! Algo deu errado.',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      viewModel.error,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(color: Colors.grey),
+                    ),
+                    const SizedBox(height: 24),
+                    ElevatedButton.icon(
+                      onPressed: () {
+                        viewModel.fetchAnimes();
+                      },
+                      icon: const Icon(Icons.refresh),
+                      label: const Text('Tentar Novamente'),
+                    ),
+                  ],
+                ),
               ),
             );
           }
 
-          // Estado Sucesso
-          return ListView.builder(
-            itemCount: viewModel.animes.length,
-            itemBuilder: (context, index) {
-              final anime = viewModel.animes[index];
-              return Card(
-                margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                child: ListTile(
-                  leading: SizedBox(
-                    width: 50,
-                    child: CachedNetworkImage(
-                      imageUrl: anime.imageUrl,
-                      placeholder: (context, url) =>
-                          const CircularProgressIndicator(),
-                      errorWidget: (context, url, error) =>
-                          const Icon(Icons.error),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  title: Text(
-                    anime.title,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: const TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  subtitle: Text('Score: ${anime.score ?? "N/A"}'),
-                  trailing: const Icon(Icons.arrow_forward_ios, size: 16),
-                  onTap: () {
-                    print('Clicou no anime: ${anime.title}');
-                  },
-                ),
-              );
+          // Estado de Sucesso (Lista com Pull-To-Refresh)
+          return RefreshIndicator(
+            onRefresh: () async {
+              await viewModel.fetchAnimes();
             },
+            color: Colors.blue,
+            child: ListView.builder(
+              physics:
+                  const AlwaysScrollableScrollPhysics(), // garantir que funcione mesmo se tiver poucos itens
+              itemCount: viewModel.animes.length,
+              itemBuilder: (context, index) {
+                final anime = viewModel.animes[index];
+                return Card(
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: 10,
+                    vertical: 5,
+                  ),
+                  child: ListTile(
+                    leading: SizedBox(
+                      width: 50,
+                      child: CachedNetworkImage(
+                        imageUrl: anime.imageUrl,
+                        placeholder: (context, url) =>
+                            const CircularProgressIndicator(),
+                        errorWidget: (context, url, error) =>
+                            const Icon(Icons.error),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    title: Text(
+                      anime.title,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text('Score: ${anime.score ?? "N/A"}'),
+                    trailing: const Icon(Icons.arrow_forward_ios, size: 17),
+                    onTap: () {
+                      print('Clicou no anime: ${anime.title}');
+                    },
+                  ),
+                );
+              },
+            ),
           );
         },
       ),
